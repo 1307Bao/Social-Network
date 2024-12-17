f78bf192574903fcf1903476b9f07413c210e524
1307Bao <7713bbbo@gmail.com>
2024-12-17 23:53:34 +0700

    update user

--- src/main/java/com/example/Social_Network/Entity/Post.java
+++ src/main/java/com/example/Social_Network/Entity/Post.java
@@ -1,9 +1,6 @@
 package com.example.Social_Network.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -21,6 +18,7 @@ public class Post {
     String post_id;
     String content;
     String user_id;
+    @Column(name = "create_at")
     Date createAt;
     String image;
 }
